569543b244adc63d6d82ce930e1112137d76600f
Melissa <ha@melissadata.com>
2023-08-25 10:03:40 -0700

    Tested successfully using Melissa data files for 2023.08

--- MelissaAddressObjectLinuxJava.sh
+++ MelissaAddressObjectLinuxJava.sh
@@ -73,7 +73,7 @@ done
 
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.07'
+RELEASE_VERSION='2023.08'
 ProductName="DQ_ADDR_DATA"
 
 # Uses the location of the .sh file 
@@ -134,7 +134,7 @@ DownloadDataFiles()
 
 DownloadSO() 
 {
-    printf "\nMELISSA UPDATER IS DOWNLOADING SO(s)...\n"
+    printf "\nMELISSA UPDATER IS DOWNLOADING SO(S)...\n"
     
     # Check for quiet mode
     if [ $quiet == "true" ];
